cfb6d9aee7ed7399b93555626bf0266bf0136353
davidfekke <david@fekke.com>
2017-01-15 14:20:31 -0500

    Adding runall script, and added promise to clean script

--- jimpsample/clean.js
+++ jimpsample/clean.js
@@ -1,48 +1,47 @@
 const fs = require('fs');
 
-var filesProcessed = 0;
-
+// See if the output folder exists
 fs.stat('../output/', (err, results) => {
     if (err) {
         if (err.code === 'ENOENT') {
-            console.log(err.error);
+            console.log('No Output folder to delete.')
         } else {
-            console.log('Second error condition');
+            console.error(err);
         }
     } else {
-        removeOutput();
+        removeFolderContentsAsync('../output/').then((d) => folderContentsDeleted(d));
     }
 });
 
-function removeOutput() {
-    fs.readdir('../output/', (err, files) => {
-        console.log(files);
-        files.forEach((f, index, array) => {
-            fs.unlink('../output/' + f, (err, result) => {
-                if (err) console.error(err);
-                filesProcessed++;
-                if (filesProcessed === array.length) {
-                    setTimeout(folderContentsDeleted, 100);
-                }
+// Delete contents of folder and return a promise once done.
+function removeFolderContentsAsync(folder) {
+    return new Promise(function(resolve, reject) {
+        fs.readdir(folder, (err, files) => {
+            files.forEach((f, index, array) => {
+                fs.unlink(folder + f, (err, result) => {
+                    if (err !== null) return reject(err);
+                    resolve(folder);
+                });
             });
         });
     });
 }
 
-
-function folderContentsDeleted() {
-    fs.rmdir('../output', (err, result) => {
+// Delete folder after contents removed
+function folderContentsDeleted(folder) {
+    fs.rmdir(folder, (err, result) => {
         if (err) console.error(err);
-        //console.log(result);
+        console.log(`${folder} folder was deleted successfully!`);
     });
 }
 
+// Delete individual files
 fs.readdir('./', (err, files) => {
     const list = files.filter(f => f.substr(-4) === '.png' || f.substr(-4) === '.jpg');
     const exludedList = list.filter(f => f !== 'lenna.png' && f !== 'dice.png');
     exludedList.forEach(i => {
         fs.unlink(i, (err) => {
-            console.error(err);
+            if (err) console.error(err);
         });
     });
 });
---
+++ jimpsample/runall.sh
@@ -0,0 +1,7 @@
+node example1.js
+node example2.js
+node example3.js
+node example4.js
+node example5.js
+node example6.js
+node example7.js
